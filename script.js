e6f2c43e58b956dccf4e246dd0eabd2f46da014a
Karla Pienaar <karlapienaar1@gmail.com>
2026-07-21 20:04:00 +0200

    Add files via upload
    Add portfolio website files

---
+++ script.js
@@ -0,0 +1,32 @@
+// Typing effect
+
+const text = "Head of Marketing • Final-Year BSc IT Student";
+const typingElement = document.querySelector(".hero h2");
+
+let i = 0;
+
+typingElement.textContent = "";
+
+function typeWriter() {
+
+    if (i < text.length) {
+
+        typingElement.textContent += text.charAt(i);
+        i++;
+
+        setTimeout(typeWriter, 60);
+
+    }
+
+}
+
+typeWriter();
+
+
+// Fade in page when loaded
+
+window.onload = function () {
+
+    document.body.style.opacity = "1";
+
+};
